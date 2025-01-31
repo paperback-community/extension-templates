fde40f939fa6fb1a6a87af138ea266aecc4d0b37
Nicholas Lim <nicknitewolf@gmail.com>
2025-01-31 13:50:48 +0800

    tools: Make template tools separate options from flags

--- src/tools/tools.ts
+++ src/tools/tools.ts
@@ -1,55 +1,53 @@
 import { argv } from "node:process";
+// Import your tools here:
 import contentTemplateTool from "./ContentTemplate/tool.js";
 
 async function run(args: string[]): Promise<void> {
-  let all = false;
-  let write = false;
+  args = args.slice(2);
 
-  args.shift();
-  args.shift();
+  const flags = new Set<string>();
+  const parameters = new Set<string>();
 
-  for (let i = args.length - 1; i >= 0; i--) {
-    switch (args[i]) {
-      case "--write":
-        write = true;
-        args.splice(i, 1);
-        break;
-      case "--all":
-        all = true;
-        args.splice(i, 1);
-        break;
+  for (const arg of args) {
+    if (arg.startsWith("-")) {
+      flags.add(arg);
+    } else {
+      parameters.add(arg);
     }
   }
 
-  if (args.length == 0) {
+  const write = flags.has("-w") || flags.has("--write");
+
+  let all = false;
+  if (parameters.size == 0) {
     all = true;
   }
 
   switch (all) {
     case true:
+      // Add your tool functions here:
       console.log(await contentTemplateTool(write, 5, 3));
       break;
 
     case false:
-      for (const arg of args) {
-        switch (arg) {
+      for (const parameter of parameters) {
+        switch (parameter) {
+          // Add your tool functions in a new switch case here:
           case "ContentTemplate":
             console.log(await contentTemplateTool(write, 5, 7));
             break;
           default:
-            console.log(`No tools to run for ${arg}`);
+            console.log(`No tools to run for parameter ${parameter}`);
         }
       }
-      break;
   }
 }
 
 try {
   await run(argv);
-
   console.log("Finished running all tools.");
   process.exit(0);
 } catch (error) {
-  console.error("An error occured while running all tools:", error);
+  console.error("An error occurred while running all tools:", error);
   process.exit(1);
 }
