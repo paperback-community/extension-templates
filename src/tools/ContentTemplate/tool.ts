0ae77e679a103d66496a46a2d1b672457251a92d
Eduard Smet <contact@celarye.dev>
2025-02-08 11:30:45 +0100

    Removed the tools and updated outdated files throughout the repository

--- src/tools/ContentTemplate/tool.ts
+++
@@ -1,8 +0,0 @@
-export default async function contentTemplateTool(
-  write: boolean,
-  a: number,
-  b: number,
-): Promise<number> {
-  void write;
-  return new Promise((resolve) => resolve(a + b));
-}
